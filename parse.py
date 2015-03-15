75d497d7d87c9359d6fa2088f5057bc97a52ed67
Michael Osl <michael.osl@kununu.com>
2015-03-15 17:57:49 +0100

    Write result to sqlite3 db

--- parse.py
+++ parse.py
@@ -3,6 +3,7 @@
 import re
 import codecs
 import sys
+import sqlite3
 from xml.sax import make_parser, handler
 
 sys.stdout = codecs.getwriter('utf8')(sys.stdout)
@@ -26,15 +27,21 @@ class Noun:
     def article(self):
         try:
             raw = re.search(r'Nominativ Singular( \d)?=(.*?)( |<)', self.text).group(2)
-            return raw.replace("(", " ").replace(")", " ").replace(" ", "")
+            return raw.replace("(", " ").replace(")", " ").replace(" ", "").lower()
         except:
             add_to_unkown_log(self)
             return "??"
 
 class DeWiktionaryXmlHandler(handler.ContentHandler):
-    def __init__(self):
+    def __init__(self, db):
         self.noun = Noun()
         self.content = ""
+        self.word_id = 0
+        self.db = db
+        c = db.cursor()
+        c.execute("DROP TABLE words")
+        c.execute('''CREATE TABLE words
+                     (_id integer primary key, article text, word text)''')
 
     def characters(self, content):
         self.content += content.strip()
@@ -53,13 +60,19 @@ class DeWiktionaryXmlHandler(handler.ContentHandler):
             self.noun.text = self.content
 
         if name == "page":
-            if self.noun.namespace is 0 and self.noun.is_german_noun():
-                print "%s,%s" % (self.noun.title, self.noun.article())
+            if self.noun.namespace is 0 and self.noun.is_german_noun() and self.noun.article() in ["der", "die", "das"]:
+                self.word_id += 1
+                self.db.execute("INSERT INTO words (_id, word, article) VALUES (?, ?, ?)", (self.word_id, self.noun.title, self.noun.article()))
+                print "%d,%s,%s" % (self.word_id, self.noun.title, self.noun.article())
                 self.noun = Noun()
 
         self.content = ""
 
+output_db = "./data/articles.db"
+conn = sqlite3.connect(output_db)
 parser = make_parser()
-b = DeWiktionaryXmlHandler()
+b = DeWiktionaryXmlHandler(conn)
 parser.setContentHandler(b)
+output_db = "./data/articles.db"
 parser.parse("data/dewiktionary.xml")
+conn.commit()
